c8085fa3135c5dbf55af82dcdd974915b0be5775
vineeth-nellore <79076402+vineeth-nellore@users.noreply.github.com>
2025-03-06 10:22:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const canvas  = document.getElementById("canvas");
+const canvasContainer = document.querySelector(".canvasContainer");
+
+const ctx = canvas.getContext('2d');
+const image = new Image();
+
+document.getElementById("upload").addEventListener("change",function(event){
+    const file=event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function(e){
+        image.src = e.target.result;
+    }
+    reader.readAsDataURL(file);
+})
+
+image.onload = function(){
+    canvas.width = image.width;
+    canvas.height = image.height;
+    ctx.drawImage(image,0,0);
+    canvasContainer.style.display='block';
+}
+
+function applyFilters(){
+    const brightness=document.getElementById("brightness").value;
+    const contrast=document.getElementById("contrast").value;
+    const grayscale=document.getElementById("grayscale").value;
+    ctx.filter =   `
+        brightness(${brightness}%)
+        contrast(${contrast}%)
+        grayscale(${grayscale}%)
+    `
+    ctx.drawImage(image,0,0);
+}
+
+document.getElementById("brightness").addEventListener("input",applyFilters);
+document.getElementById("contrast").addEventListener("input",applyFilters);
+document.getElementById("grayscale").addEventListener("input",applyFilters);
+
+document.getElementById("download").addEventListener("click",function(){
+    const link = document.createElement("a");
+    link.href=canvas.toDataURL();
+    link.download='edited-image.png';
+    link.click();
+})
